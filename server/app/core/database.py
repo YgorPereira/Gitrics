3423871dfa80a58cf1925dc4fcc4b4b2615682e2
YgorPereira <ygorrp25@gmail.com>
2026-07-25 22:27:15 -0300

    server-config: database session factory

---
+++ server/app/core/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from loguru import logger
+
+from app.core import settings
+
+engine = create_async_engine(
+    url=settings.database_url,
+    echo= settings.DEBUG,
+)
+
+SessionFactory = async_sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
+
+async def get_db():
+    async with SessionFactory() as db:
+        try:
+            yield db
+        except Exception:
+            logger.exception("Error on database session - running rollback")
+            await db.rollback()
+            raise
--- server/app/core/settings.py
+++ server/app/core/settings.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -6,6 +6,32 @@ class Settings(BaseSettings):
     APP_NAME: str = "Gitrics"
     APP_DESCRIPTION: str = "Git smart analyzer"
     APP_VERSION: str = "1.0.0"
+
+    # postgre config props (from .env)
+    PSGRE_USER: str = "postgres"
+    PSGRE_PASSWORD: str
+    PSGRE_HOST: str = "localhost"
+    PSGRE_PORT: int = 5432
+    PSGRE_DB: str = "gitrics"
+
+    # debug boolean from develop enviroment
+    DEBUG: bool = False
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+    @property
+    def database_url(self):
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.PSGRE_USER}:"
+            f"{self.PSGRE_PASSWORD}@"
+            f"{self.PSGRE_HOST}:"
+            f"{self.PSGRE_PORT}/"
+            f"{self.PSGRE_DB}"
+        )
 
 
 settings = Settings()
