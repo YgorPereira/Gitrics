d1795f58954901957adc9e70ef70e5766c088f24
YgorPereira <ygorrp25@gmail.com>
2026-08-01 20:45:07 -0300

    client-feat: add apí communication

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
+import { login } from "@/features/auth/services/authService";
 import { AppButton } from "@/shared/components";
 
 export function HomePage() {
  return (
     <div>
-        <AppButton variant="contained">
+        <AppButton variant="contained" onClick={() => login()}>
             Entrar com github
         </AppButton>
     </div>
